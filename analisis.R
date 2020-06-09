8ddc54f3076ea5d9ee4b75a95fae03d6fe2a8b0b
Julian Giraldo <julian.giraldo.h@gmail.com>
2020-06-09 11:47:40 -0500

    Actualiza código para graficar las series

--- analisis.R
+++ analisis.R
@@ -1,21 +1,18 @@
 library(tidyverse)
 library(plotly)
 
-data <- read_csv("./datos_torreajedrez.csv")
+data <- read_csv("./data/datos_torreajedrez.csv")
+
+colnames(data)
 
 data$sta_siata.mean_pm25 <- data$sta_siata.mean_pm25 %>% lag(4)
 data$sta_siata.mean_pm25[data$sta_siata.mean_pm25 <  -100] <- -1
+data$date <- lubridate::mdy_hms(data$date)
 
-data_g <- gather(data, -time,key = "sensor", value = "valor")
+data_g <- gather(data, -date,key = "sensor", value = "valor")
 
-gg <- ggplot(data_g, aes(time, valor, color=sensor)) + geom_line(stat = "identity")
+gg <- ggplot(data_g %>%
+             filter(date > "2019-12-01", date < "2020-04-01"),
+             aes(date, valor, color=sensor)) + geom_line(stat = "identity")
 ggplotly(gg)
 
-
-g <- ggplot(data, aes(time)) +
-    geom_line(aes(y = sta_siata.mean_pm25, colour = "red"), stat = "identity") +
-    geom_line(aes(y = aqa_explora_1.mean_pm25, colour = "blue"), stat = "identity") +
-    geom_line(aes(y = aqa_explora_2.mean_pm25, colour = "green"), stat = "identity") +
-    geom_line(aes(y = aqa_explora_3.mean_pm25, colour = "purple"), stat = "identity")
-
-ggploty(g)
